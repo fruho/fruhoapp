e7d791450c768eb059dc8620d640d4a1fdfbc799
hypatia2 <hypatia2@yandex.com>
2015-08-23 14:57:45 +0200

    ruby version change

--- bootstrap.tcl
+++ bootstrap.tcl
@@ -70,8 +70,8 @@ proc install-fpm {} {
     if {[catch {exec fpm --version}] == 1} {
         puts "Installing fpm"
         ex sudo apt-get update --fix-missing
-        ex sudo apt-get -fy install git ruby-dev gcc rpm
-        catch {ex sudo apt-get -fy install ruby1.8-dev}
+        catch {ex sudo apt-get remove ruby1.8}
+        ex sudo apt-get -fy install git ruby1.9.1-full ruby-dev gcc rpm
         ex sudo apt-get -fy install rubygems
         ex sudo apt-get -fy install rubygems-integration
         ex sudo gem install fpm
